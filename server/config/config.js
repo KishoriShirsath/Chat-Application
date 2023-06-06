8c7f65d2945c9e6cfa636997be3921acf3cd108e
Kishori Shirsath <kishorishirsath1998@gmail.com>
2023-06-06 08:52:47 +0530

    user root

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 require("dotenv").config("../.env");
 
 module.exports = {
-  username: "default",
+  username: "root",
   password: "kSlVz0MB2otu",
   database: "verceldb",
   host: "ep-young-violet-330131-pooler.us-east-1.postgres.vercel-storage.com",
