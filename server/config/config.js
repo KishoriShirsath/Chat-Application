5fc225e1956f4dda5d8d13c575a939e099fbbd09
Kishori Shirsath <kishorishirsath1998@gmail.com>
2023-06-06 23:08:45 +0530

    mysql server connection

--- server/config/config.js
+++ server/config/config.js
@@ -1,16 +1,32 @@
 require("dotenv").config("../.env");
 
 module.exports = {
-  username: "root",
-  password: "kSlVz0MB2otu",
-  database: "verceldb",
-  host: "ep-young-violet-330131-pooler.us-east-1.postgres.vercel-storage.com",
-  dialect: "postgres",
-  logging: true,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: true, // You can set this to false if needed
-    },
-  },
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+  port: process.env.DB_PORT,
+  logging: false,
+  // dialectOptions: {
+  //   ssl: {
+  //     require: true,
+  //     rejectUnauthorized: true, // You can set this to false if needed
+  //   },
+  // },
 };
+
+// module.exports = {
+//   username: "root",
+//   password: "kSlVz0MB2otu",
+//   database: "verceldb",
+//   host: "ep-young-violet-330131-pooler.us-east-1.postgres.vercel-storage.com",
+//   dialect: "postgres",
+//   logging: true,
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: true, // You can set this to false if needed
+//     },
+//   },
+// };
